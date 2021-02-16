d54ac129b82e8d9e096d98e3c3120381fcfa89cd
nonodamiens <nonodamiens@gmail.com>
2021-02-16 16:21:29 +0100

    Add route characters

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def hello_orld():
 	return "Hello world !"
+
+@app.route("/characters/")
+def characters():
+	characters = {
+	1:"Bob",
+	2:"John"
+	}
+	return jsonify(characters)
